b8eab429d20c13f9cedfb3390e0846ed517c34d7
kyushun <27068326+kyushun@users.noreply.github.com>
2023-06-13 07:43:35 +0900

    feat: abbrTimezone

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,27 @@
-export function add(a: number, b: number) {
-  return a + b;
-}
+import type { PluginFunc } from 'dayjs';
+import timezones from 'timezones.json';
+import type timezone from 'dayjs/plugin/timezone';
+
+export const abbrTimezone: PluginFunc = (_, dayjsClass) => {
+  const originalFormat = dayjsClass.prototype.format;
+
+  dayjsClass.prototype.format = function (template?: string | undefined) {
+    const result = template?.replace(/\[([^\]]+)]|tz/g, (match) => {
+      switch (match) {
+        case 'tz':
+          const unabbrZoneName = this.offsetName('long');
+          const timezone = timezones.find(
+            (timezone) => timezone.value == unabbrZoneName,
+          );
+
+          return timezone?.abbr ?? match;
+        default:
+          return match;
+      }
+    });
+
+    return originalFormat.bind(this)(result);
+  };
+};
+
+export default abbrTimezone;
