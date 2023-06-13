af7a503c61ead93e1bac153977434f78ddd2cba6
kyushun <27068326+kyushun@users.noreply.github.com>
2023-06-13 13:21:03 +0900

    chore: fix eslintrc

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,11 +2,9 @@ import dayjs from 'dayjs';
 import timezone from 'dayjs/plugin/timezone';
 import utc from 'dayjs/plugin/utc';
 
-// eslint-disable-next-line import/no-unresolved
 import { abbrTimezone } from '.';
 
 dayjs.extend(utc);
-
 dayjs.extend(timezone);
 dayjs.extend(abbrTimezone);
 
